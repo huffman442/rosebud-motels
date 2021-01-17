8366587d865e8f36636aa68ac0009c16c697dc53
huffman442 <huffman442@yahoo.com>
2021-01-17 14:48:01 -0500

    add title test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Rosebud Motels"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Rosebud Motels"
   end
 end

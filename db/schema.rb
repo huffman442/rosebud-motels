c7b81485a94f6a5ed5b41e1cfbbe0c1cffa539c2
huffman442 <huffman442@yahoo.com>
2021-02-02 16:57:18 -0500

    Add in final parts of scaffolding locations
    This was primarily done to make the new locations controller test file
    pass.
    
    Also added a validation for the presence of the 'name' field.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_030321) do
+ActiveRecord::Schema.define(version: 2021_01_28_013049) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "zip_code"
+    t.string "state"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
